47451f86a890b70296356160c9a294c6b18ac37b
Minhaas <minhaasmohammed@gmail.com>
2022-03-24 20:20:05 -0400

    Final model

--- resnet_18.py
+++ resnet_18.py
@@ -12,18 +12,9 @@ from collections import OrderedDict
 from cutout import Cutout
 import csv
 import pandas as pd
-# from lookahead import Lookahead
+from project1_model import project1_model
 
 data_statistics = ([0.4914, 0.4822, 0.4465],[0.2023,0.1994,0.2010])
-# train_transforms_cifar = transforms.Compose([
-#     transforms.RandomCrop(32, padding=4, padding_mode='reflect'),
-#     transforms.RandomHorizontalFlip(),
-#     #transforms.Resize(256),
-#     # transforms.CenterCrop(224),
-#     transforms.ToTensor(),
-#     transforms.Normalize(*data_statistics, inplace=True),
-#     Cutout(n_holes=1, length=8)
-# ])
 
 test_transform_cifar = transforms.Compose([
     transforms.ToTensor(),
@@ -36,8 +27,6 @@ aug_dataset = init_dataset
 aug_dataset.transform= transforms.Compose([
     transforms.RandomCrop(32, padding=4, padding_mode='reflect'),
     transforms.RandomHorizontalFlip(),
-    # transforms.Resize(256),
-    # transforms.CenterCrop(224),
     transforms.ToTensor(),
     transforms.Normalize(*data_statistics, inplace=True),
     Cutout(n_holes=1, length=8)
@@ -52,18 +41,15 @@ val_dl = DataLoader(val_dataset, batch_size, shuffle = True, pin_memory = True)
 test_dl = DataLoader(test_dataset, batch_size, shuffle = True, pin_memory = True)
 
 def denormalizer(images, means, std_devs):
-    #(image*st_dev + mean
     means = torch.tensor(means).reshape(1,3,1,1)
     std_devs = torch.tensor(std_devs).reshape(1,3,1,1)
     return images * std_devs + means
-#showing the images
+
 def show_batch(dl):
     for images, labels in dl:
         fig, ax = plt.subplots(figsize = (10,10))
         images = denormalizer(images, *data_statistics)
-        # ax.imshow(make_grid(images,10).permute(1,2,0))
         fig.savefig('images.png')
-        # print(labels)
         break
         
 show_batch(train_dl)
@@ -95,75 +81,74 @@ train_dl = DeviceDataLoader(train_dl, device)
 val_dl = DeviceDataLoader(val_dl, device)
 test_dl = DeviceDataLoader(test_dl, device)
 
-class BasicBlock(nn.Module):
+# class BasicBlock(nn.Module):
 
-    def __init__(self, in_planes, planes, stride=1):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
+#     def __init__(self, in_planes, planes, stride=1):
+#         super(BasicBlock, self).__init__()
+#         self.conv1 = nn.Conv2d(
+#             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+#         self.bn1 = nn.BatchNorm2d(planes)
+#         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+#                                stride=1, padding=1, bias=False)
+#         self.bn2 = nn.BatchNorm2d(planes)
 
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, planes,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes)
-            )
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
+#         self.shortcut = nn.Sequential()
+#         if stride != 1 or in_planes != planes:
+#             self.shortcut = nn.Sequential(
+#                 nn.Conv2d(in_planes, planes,
+#                           kernel_size=1, stride=stride, bias=False),
+#                 nn.BatchNorm2d(planes)
+#             )
+#     def forward(self, x):
+#         out = F.relu(self.bn1(self.conv1(x)))
+#         out = self.bn2(self.conv2(out))
+#         out += self.shortcut(x)
+#         out = F.relu(out)
+#         return out
 
-class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
-        super(ResNet, self).__init__()
-        self.in_planes = 64
+# class ResNet(nn.Module):
+#     def __init__(self, block, num_blocks, num_classes=10):
+#         super(ResNet, self).__init__()
+#         self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-                               stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512, num_classes)
-        # self.dropout = nn.Dropout(0.25)
+#         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+#                                stride=1, padding=1, bias=False)
+#         self.bn1 = nn.BatchNorm2d(64)
+#         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+#         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+#         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+#         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+#         self.linear = nn.Linear(512, num_classes)
+#         # self.dropout = nn.Dropout(0.25)
   
-    def _make_layer(self, block, planes, num_blocks, stride):
-        downsample = None
-        strides = [stride] + [1]*(num_blocks-1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes
-        return nn.Sequential(*layers)
-    #print("x")
+#     def _make_layer(self, block, planes, num_blocks, stride):
+#         downsample = None
+#         strides = [stride] + [1]*(num_blocks-1)
+#         layers = []
+#         for stride in strides:
+#             layers.append(block(self.in_planes, planes, stride))
+#             self.in_planes = planes
+#         return nn.Sequential(*layers)
 
 
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out
+#     def forward(self, x):
+#         out = F.relu(self.bn1(self.conv1(x)))
+#         out = self.layer1(out)
+#         out = self.layer2(out)
+#         out = self.layer3(out)
+#         out = self.layer4(out)
+#         out = F.avg_pool2d(out, 4)
+#         out = out.view(out.size(0), -1)
+#         out = self.linear(out)
+#         return out
 
 
-def resnet18():
-    layers=[1, 1, 1, 1]
-    model = ResNet(BasicBlock, layers)
-    return model
+# def resnet18():
+#     layers=[1, 1, 1, 1]
+#     model = ResNet(BasicBlock, layers)
+#     return model
 
-model = resnet18()
+model = project1_model()
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -216,10 +201,9 @@ def train(model, train_dl, val_dl, epochs, max_lr, loss_func, optim):
                 logits = model(images)
             batch_losses.append(loss_func(logits, labels))
             batch_accs.append(accuracy(logits, labels))
-           # print("batch_vld_acc", accuracy(logits, labels))
         epoch_avg_loss, epoch_avg_acc = evaluate(model, val_dl, loss_func)
         results.append({'avg_valid_loss': epoch_avg_loss, "avg_valid_acc": epoch_avg_acc, "avg_train_loss" : epoch_train_loss, "lrs" : lrs})
-        # print(f"Average loss: {epoch_avg_loss}, Average accuracy {epoch_avg_acc}, Training loss: {epoch_train_loss}")
+        print(f"Average loss: {epoch_avg_loss}, Average accuracy {epoch_avg_acc}, Training loss: {epoch_train_loss}")
     return results
 
 epochs = 10
@@ -249,32 +233,6 @@ def plot(results, pairs):
     
 plot(results, [{"accuracy_vs_epochs": ["avg_valid_acc"]}, {"Losses_vs_epochs" : ["avg_valid_loss", "avg_train_loss"]}, {"learning_rates_vs_batches": ["lrs"]}])
 
-test_loss_history = []
-train_loss_history = []
-for epoch in range(epochs):
-    train_loss = 0.0
-    test_loss = 0.0
-    for i, data in enumerate(train_dl):
-        with torch.no_grad():
-            images, labels = data
-            # optim.zero_grad()
-            predicted_output = model(images)
-            fit = loss_func(predicted_output,labels)
-            fit.backward()
-            optim.step()
-            train_loss += fit.item()
-    for i, data in enumerate(test_dl):
-        with torch.no_grad():
-            images, labels = data
-            predicted_output = model(images)
-            fit = loss_func(predicted_output,labels)
-            test_loss += fit.item()
-    train_loss = train_loss/len(train_dl)
-    test_loss = test_loss/len(test_dl)
-    test_loss_history.append(test_loss)
-    train_loss_history.append(train_loss)
-    print('Epoch %s, Train loss %s, Test loss %s',(epoch, train_loss, test_loss))
-
 _,test_acc=evaluate(model,test_dl,loss_func)
 params = count_parameters(model)
 print(f"Test accuracy is {test_acc*100} %")
@@ -287,8 +245,4 @@ with open("stats_adam.csv") as op_file:
     fc.writeheader()
     fc.writerows(results)
 
-test_loss_df = pd.DataFrame(test_loss_history)
-test_loss_df.to_csv("test_loss.csv")
-train_loss_df = pd.DataFrame(train_loss_history)
-train_loss_df.to_csv("train_loss.csv")
 
